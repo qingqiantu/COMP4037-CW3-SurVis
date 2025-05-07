b2b914dee75e1679d7d39d13220abdd90d5be87e
qingqiantu <tqq94666@163.com>
2025-05-08 02:33:22 +0800

    Update available_img.js

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -1,1 +1,13 @@
-const availableImg = ["Beck2016Visual"];
+const availableImg = {
+  "Banerjee2012Coalition": "data/generated/papers_img/Banerjee2012Coalition.png",
+  "Beck2016Visual": "data/generated/papers_img/Beck2016Visual.png",
+  "Becker2021Achievement": "data/generated/papers_img/Becker2021Achievement.png",
+  "DeTroyer2019TrueBiters": "data/generated/papers_img/DeTroyer2019TrueBiters.png",
+  "MayerEichberger2020QBFGames": "data/generated/papers_img/MayerEichberger2020QBFGames.png",
+  "Schaefer2013LogicGame": "data/generated/papers_img/Schaefer2013LogicGame.png",
+  "Scholl2018DQBF": "data/generated/papers_img/Scholl2018DQBF.png",
+  "Schuppan2018Cores": "data/generated/papers_img/Schuppan2018Cores.png",
+  "Shaik2023SQVal.png": "data/generated/papers_img/Shaik2023SQVal.png",
+  "Shukla2019QBF": "data/generated/papers_img/Shukla2019QBF.png",
+  "Weng2020PacmanLogic.png": "data/generated/papers_img/Weng2020PacmanLogic.png"
+};
