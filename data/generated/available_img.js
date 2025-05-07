3778f2c19405833407aa045991439afdc6fb4c00
qingqiantu <tqq94666@163.com>
2025-05-08 02:39:31 +0800

    Update available_img.js

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -1,13 +1,1 @@
-const availableImg = [
-  "Banerjee2012Coalition",
-  "Beck2016Visual",
-  "Becker2021Achievement",
-  "DeTroyer2019TrueBiters",
-  "MayerEichberger2020QBFGames",
-  "Schaefer2013LogicGame",
-  "Scholl2018DQBF",
-  "Schuppan2018Cores",
-  "Shaik2023SQVal",
-  "Shukla2019QBF",
-  "Weng2020PacmanLogic"
-];
+const availableImg = ["Beck2016Visual"];
